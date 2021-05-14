e5ab3d316d7bfcc2395284cc991a5ace38f391ca
Sreerajkc8 <sreerajkc8@gmail.com>
2021-05-14 17:39:56 +0530

    Walk AI completed
    Walk AI with height and distance check completed

---
+++ Assets/WalkAI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkAI: MonoBehaviour
+{
+    [Header("Walking Behaviour")]
+    public float walkSpeed=2;
+    public float maxDistance=1.5f;
+    public float maxDepth=2f;
+    private float distanceToObjectSide;
+    private float distanceToGround;
+    private bool canGoRight=false;
+    private bool isInAlert=false;
+
+    RaycastHit2D hit2Dside,hit2Ddown;
+    public Transform raycastOrigin;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+    }
+
+    private void Walk()
+    {
+        if(distanceToObjectSide>=maxDistance && distanceToGround <= maxDepth)
+        {
+            transform.Translate(transform.TransformDirection(transform.right)*walkSpeed*Time.deltaTime);
+        }
+        else
+        {
+            canGoRight=!canGoRight;
+            if(canGoRight)
+            {
+                transform.eulerAngles=new Vector3(0,0,0);
+            }
+            else
+            {
+                transform.eulerAngles=new Vector3(0,180,0);
+            }
+        }
+    }
+    private void FixedUpdate() 
+    {   
+        RaycastSide();
+        RaycastDown();
+    }
+    private void RaycastSide()// Magnitude is the direction sign
+    {
+        hit2Dside=Physics2D.Raycast(raycastOrigin.position,transform.TransformDirection(Vector2.right),LayerMask.NameToLayer("Collidable"));
+        distanceToObjectSide=Vector2.Distance(transform.position,hit2Dside.point);
+    }
+    private void RaycastDown()
+    {
+        hit2Ddown=Physics2D.Raycast(raycastOrigin.position,transform.TransformDirection(Vector2.down));
+        distanceToGround=Vector2.Distance(transform.position,hit2Ddown.point);
+    }
+
+}
